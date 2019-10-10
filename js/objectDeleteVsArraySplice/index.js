487992f794b51bd71b8060c68e2bd1c611aa2fa1
harryli0088 <harryli0088@gmail.com>
2019-10-10 11:22:34 -0400

    working js files, untested objectDeleteVsArraySplice and weightedShuffle

--- js/objectDeleteVsArraySplice/index.js
+++ js/objectDeleteVsArraySplice/index.js
@@ -1,29 +1,40 @@
-let num = 100000;
+const Tester = require("../Tester.js");
 
 
-let start = new Date();
 
-let array = []; //initialize empty array
-for(let i=0; i<num; ++i) {
-  array.push({}); //push empty objects
-}
-console.log("Array done pushing", new Date() - start);
-while(array.length > 0) {
-  array.splice(0, 1); //splice out objects one at a time
-}
-
-console.log("Array pushing and splicing", new Date() - start);
-
-
-start = new Date();
-
-let obj = {}; //initialize empty object
-for(let i=0; i<num; ++i) {
-  obj[i] = {}; //add empty objects to object
-}
-console.log("Object done setting", new Date() - start);
-for(let field in obj) {
-  delete obj[field]; //delete objects
+function arraySplice(num) {
+  let array = []; //initialize empty array
+  for(let i=0; i<num; ++i) {
+    array.push({}); //push empty objects
+  }
+  while(array.length > 0) {
+    array.splice(0, 1); //splice out objects one at a time
+  }
 }
 
-console.log("Object field setting and deleting", new Date() - start);
+function objectDelete(num) {
+  let obj = {}; //initialize empty object
+  for(let i=0; i<num; ++i) {
+    obj[i] = {}; //add empty objects to object
+  }
+  for(let field in obj) {
+    delete obj[field]; //delete objects
+  }
+}
+
+
+
+const test = new Tester({
+  maxNum: 1000000,
+  maxIter: 10,
+  function1: {
+    run: arraySplice,
+    heading: "Array Splice"
+  },
+  function2: {
+    heading: "Object Delete",
+    run: objectDelete
+  }
+});
+
+test.run();
--- js/weightedShuffle/index.js
+++ js/weightedShuffle/index.js
@@ -1,56 +1,73 @@
-const number = 1000000;
-
-//initialize array to be used
-let array = [];
-let sum = 0;
-for(let i=0; i<number; ++i) {
-  let rand = Math.floor( Math.random()*10 ) + 1; //generate a random number 1 - 10
-  array.push({index: i, count: rand}); //push object into array
-  sum += rand; //sum the random numbers generated
-}
-console.log("Array length: ",array.length,"; Sum: ", sum);
+const Tester = require("../Tester.js");
 
 
+function createArray(num) {
+  //initialize array to be used
+  let array = [];
 
-let arr = JSON.parse(JSON.stringify(array)); //copy the array before starting the time
-let i = 0;
-let start = new Date();
-//while there are still elements to be chosen
-while(i < sum) {
-  let randIndex = Math.floor( Math.random()*arr.length ); //get random element in array
-  arr[randIndex].count--; //decrement the number of times this element has been randomly chosen
-  //if this element is finished being chosen
-  if(arr[randIndex].count <= 0) {
-    arr.splice(randIndex, 1); //remove element from array entirely
+  let sum = 0;
+  for(let i=0; i<num; ++i) {
+    let rand = Math.floor( Math.random()*10 ) + 1; //generate a random number 1 - 10
+    array.push({index: i, count: rand}); //push object into array
+    sum += rand; //sum the random numbers generated
   }
 
-  i++;
+  return {array: array, sum: sum};
 }
 
-console.log("Splicing finished elements out of array: ", new Date() - start);
-
-
-
-
-arr = JSON.parse(JSON.stringify(array)); //copy the array before starting the time
-i = 0;
-start = new Date();
-//while there are still elements to be chosen
-while(i < sum) {
-  let randIndex = Math.floor( Math.random()*arr.length ); //get random element in array
-
-  //if this element is already done being chosen
-  while(arr[randIndex].count < 1) {
-    randIndex++; //move to the next element
-    //wrap around to the beginning of the array if necessary
-    if(randIndex >= arr.length) {
-      randIndex = 0;
+function arraySplice(num, preFunctionReturn) {
+  let arr = JSON.parse(JSON.stringify(preFunctionReturn.array)); //copy the array before starting the time
+  let i = 0;
+  //while there are still elements to be chosen
+  while(i < preFunctionReturn.sum) {
+    let randIndex = Math.floor( Math.random()*arr.length ); //get random element in array
+    arr[randIndex].count--; //decrement the number of times this element has been randomly chosen
+    //if this element is finished being chosen
+    if(arr[randIndex].count <= 0) {
+      arr.splice(randIndex, 1); //remove element from array entirely
     }
+
+    i++;
   }
-
-  arr[randIndex].count--; //decrement the number of times this element has been randomly chosen
-
-  i++;
 }
 
-console.log("Skipping over to the next available element: ", new Date() - start);
+function skipping(num, preFunctionReturn) {
+  let arr = JSON.parse(JSON.stringify(preFunctionReturn.array)); //copy the array before starting the time
+  i = 0;
+  start = new Date();
+  //while there are still elements to be chosen
+  while(i < preFunctionReturn.sum) {
+    let randIndex = Math.floor( Math.random()*arr.length ); //get random element in array
+
+    //if this element is already done being chosen
+    while(arr[randIndex].count < 1) {
+      randIndex++; //move to the next element
+      //wrap around to the beginning of the array if necessary
+      if(randIndex >= arr.length) {
+        randIndex = 0;
+      }
+    }
+
+    arr[randIndex].count--; //decrement the number of times this element has been randomly chosen
+
+    i++;
+  }
+}
+
+
+
+const test = new Tester({
+  maxNum: 1000000,
+  maxIter: 10,
+  preFunction: createArray,
+  function1: {
+    run: arraySplice,
+    heading: "Array splice"
+  },
+  function2: {
+    heading: "Skipping",
+    run: skipping
+  }
+});
+
+test.run();
